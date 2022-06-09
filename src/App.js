baf15f312f14aa54e89af679b40db28ecbab8187
muhammad-jiku <muhammadjiku364@gmail.com>
2022-06-09 15:51:01 +0600

    react router added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Footer from './pages/Shared/Footer/Footer';
 import Header from './pages/Shared/Header/Header';
 import Home from './pages/Home/Home/Home';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
       <Footer />
     </div>
   );
