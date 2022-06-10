b561bb1e7bc20aac8acec6cc2d66c63b7a458f50
muhammad-jiku <muhammadjiku364@gmail.com>
2022-06-10 13:16:31 +0600

    hook form added

--- src/pages/Home/Contact/Contact.js
+++ src/pages/Home/Contact/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>contact</div>;
+  return <div className="container mx-auto px-6 my-16">contact</div>;
 };
 
 export default Contact;
