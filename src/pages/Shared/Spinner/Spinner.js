817fdadf9f8303e92bf75e2023635894bad64c06
muhammad-jiku <muhammadjiku364@gmail.com>
2022-06-12 21:58:29 +0600

    final commit

--- src/pages/Shared/Spinner/Spinner.js
+++ src/pages/Shared/Spinner/Spinner.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import loading from '../../../Images/loading.gif';
 
 const Spinner = () => {
-  return <div>spinner</div>;
+  return (
+    <div className="hero min-h-full">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <img src={loading} alt="loading" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Spinner;
